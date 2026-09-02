257284888be72f4728ab00217f469950fa8f4888
Matheus Bomfim <matheusbhz@uol.com.br>
2026-09-01 21:04:15 -0300

    Add missing PBS: 1_download_data.pbs, 2_build_gene_bed.pbs, fix submit_all.sh

--- 6_variants_analysis/6.3_STRs_analysis_per_geneANDburden/6.4.1_dbscan_subset_GWAS/6.4.1.1_covid19hg_overlap/submit_all.sh
+++ 6_variants_analysis/6.3_STRs_analysis_per_geneANDburden/6.4.1_dbscan_subset_GWAS/6.4.1.1_covid19hg_overlap/submit_all.sh
@@ -12,13 +12,13 @@
 #   qsub overlap_str_cohort.pbs
 cd "$(dirname "$0")"
 
-D=$(qsub download_data.pbs)
+D=$(qsub 1_download_data.pbs)
 echo "download        -> $D"
-B=$(qsub -W depend=afterok:$D build_gene_bed.pbs)
+B=$(qsub -W depend=afterok:$D 2_build_gene_bed.pbs)
 echo "build_gene_bed  -> $B"
-E=$(qsub -W depend=afterok:$B extract_covid_genes.pbs)
+E=$(qsub -W depend=afterok:$B 3_extract_covid_genes.pbs)
 echo "extract_genes   -> $E"
-O=$(qsub -W depend=afterok:$E overlap_str_cohort.pbs)
+O=$(qsub -W depend=afterok:$E 4_overlap_str_cohort.pbs)
 echo "overlap         -> $O"
 echo
 echo "Acompanhe com:  qstat -u $USER"
